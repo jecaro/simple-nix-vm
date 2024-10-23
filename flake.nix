3c41bfd98ddeb4958328a25064da4a830f96027d
Simon Kohlmeyer <simon.kohlmeyer@gmail.com>
2024-10-23 13:01:07 +0200

    Allow nix run

--- flake.nix
+++ flake.nix
@@ -5,12 +5,18 @@
     nixpkgs.url = "github:nixos/nixpkgs/nixos-24.05";
   };
 
-  outputs = { nixpkgs, ... }:
+  outputs =
+    { nixpkgs, self, ... }:
     {
+      apps."x86_64-linux" = {
+        default = {
+          type = "app";
+          program = "${self.nixosConfigurations.vm.config.system.build.vm}/bin/run-nixos-vm";
+        };
+      };
       nixosConfigurations.vm = nixpkgs.lib.nixosSystem {
         system = "x86_64-linux";
         modules = [ ./vm.nix ];
       };
     };
 }
-
