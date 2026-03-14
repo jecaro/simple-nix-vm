542287f36a5d56c5a747252c5dad0cfed4c7eadc
jecaro <jeancharles.quillet@gmail.com>
2026-03-14 14:15:21 +0100

    Clean up

--- flake.nix
+++ flake.nix
@@ -8,12 +8,6 @@
   outputs =
     { nixpkgs, self, ... }:
     {
-      apps."x86_64-linux" = {
-        default = {
-          type = "app";
-          program = "${self.nixosConfigurations.vm.config.system.build.vm}/bin/run-nixos-vm";
-        };
-      };
       nixosConfigurations.vm = nixpkgs.lib.nixosSystem {
         system = "x86_64-linux";
         modules = [ ./vm.nix ];
